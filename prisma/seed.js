15387970b3240606956ec99b26a5ee9db7e77a65
Prince <princesuke@gmail.com>
2026-07-24 16:44:28 +0700

    feat: seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,28 @@
 import { faker } from "@faker-js/faker";
-import prisma from "../src/lib/prisma.js"
+import prisma from "../src/lib/prisma.js";
 
 async function main() {
-    await prisma.user.createMany({
-        data: Array.from({ length: 20}).map(()=>({
-            name: faker.person.fullName(),
-            email: faker.internet.email()
-        }))
-    })
+//   await prisma.$transaction([
+//     prisma.$executeRawUnsafe("TRUNCATE TABLE Comment"),
+//     prisma.$executeRawUnsafe("TRUNCATE TABLE Post"),
+//     prisma.$executeRawUnsafe("TRUNCATE TABLE User"),
+//   ]);
+
+  await prisma.$executeRawUnsafe("TRUNCATE TABLE User")
+
+  await prisma.user.createMany({
+    data: Array.from({ length: 20 }).map(() => ({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+    })),
+  });
 }
 
 main()
-    .catch((err)=> {
-        console.error(err)
-        process.exit(1)
-    })
-    .finally(async ()=> {
-        await prisma.$disconnect()
-    })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
